4053a1fa1ce21da84e6271cf572228ad71203eff
mirahafiz281@gmail.com <mirahafiz281@gmail.com>
2025-10-16 10:22:29 +0800

    First

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#Write a function to check a number is odd or even
+def check_odd_even(number):
+    if(number%2==0):
+        print('Even Number')
+    else:
+        print('Odd Number')
+
+given_number=int(input('Enter Number: '))
+check_odd_even(given_number)
